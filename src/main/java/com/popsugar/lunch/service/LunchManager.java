7f43e08f6a790b5afcdb808bae75434658412e46
Benjamin Possolo <bpossolo@gmail.com>
2015-09-30 11:45:00 -0700

    set group type as regular when creating user

--- src/main/java/com/popsugar/lunch/service/LunchManager.java
+++ src/main/java/com/popsugar/lunch/service/LunchManager.java
@@ -54,6 +54,9 @@ public class LunchManager {
 	public User createUser(String name, String email, Location location) {
 		PingboardUser pingboardUser = pingboard.getUserByEmail(email);
 		User user = new User(name, email, location);
+		List<GroupType> groups = new ArrayList<>(1);
+		groups.add(GroupType.Regular);
+		user.setGroupTypes(groups);
 		if (pingboardUser != null) {
 			user.setPingboardId(pingboardUser.getId());
 			user.setPingboardAvatarUrlSmall(pingboardUser.getAvatarUrlSmall());
