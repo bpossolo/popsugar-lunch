2e1558be4be8caf5a677a0dc16e88479c1b8d4df
Benjamin Possolo <bpossolo@gmail.com>
2015-09-29 21:20:37 -0700

    add upgrade task that adds group type to all users

---
+++ src/main/java/com/popsugar/lunch/upgrade/Task2.java
@@ -0,0 +1,30 @@
+package com.popsugar.lunch.upgrade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.popsugar.lunch.dao.UserDAO;
+import com.popsugar.lunch.model.GroupType;
+
+public class Task2 extends UpgradeTask {
+
+	@Override
+	public void run() {
+		
+		List<GroupType> groups = new ArrayList<>(1);
+		groups.add(GroupType.Regular);
+		
+		Query q = new Query(UserDAO.Kind);
+		Iterable<Entity> results = datastore.prepare(q).asIterable();
+		List<Entity> entities = new ArrayList<>();
+		for (Entity entity : results) {
+			entity.setProperty(UserDAO.GroupTypesProp, groups);
+			entities.add(entity);
+		}
+		
+		datastore.put(entities);
+	}
+
+}
