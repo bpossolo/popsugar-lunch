e54966ee081c3ee8db969621f0d1818f3674a56b
Benjamin Possolo <bpossolo@gmail.com>
2015-10-04 20:30:16 -0700

    set created date in upgrade task

--- src/main/java/com/popsugar/lunch/upgrade/Task3.java
+++ src/main/java/com/popsugar/lunch/upgrade/Task3.java
@@ -1,5 +1,6 @@
 package com.popsugar.lunch.upgrade;
 
+import java.util.Date;
 import java.util.List;
 
 import com.google.appengine.api.datastore.Entity;
@@ -11,10 +12,12 @@ public class Task3 extends UpgradeTask {
 
 	@Override
 	public void run() {
+		Date now = new Date();
 		Query q = new Query(LunchGroupDAO.Kind);
 		List<Entity> entities = datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
 		for (Entity e : entities) {
 			e.setProperty(LunchGroupDAO.ActiveProp, false);
+			e.setProperty(LunchGroupDAO.CreatedProp, now);
 		}
 		datastore.put(entities);
 	}
